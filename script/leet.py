2896919389f8ae93a9c81e45e0acb1fdd475af48
sBondoc <sabondoc@uci.edu>
2022-03-13 17:33:02 -0700

    Added prompt for remote source.

--- script/leet.py
+++ script/leet.py
@@ -11,8 +11,7 @@ from pathlib import Path
 
 LOCAL = 'C'
 DIR_LOCAL = 'Users/Sam/Desktop'
-REMOTE = 'eecs.uci.edu'
-DIR_REMOTE = 'users/ugrad/2020/winter/sabondoc/dev/leet'
+DIR_REMOTE = 'dev/leet'
 SOLUTION = 'solution'
 
 LANGUAGES = {
@@ -20,7 +19,12 @@ LANGUAGES = {
 	'py': 'py',
 }
 
-def transfer(sol: int, lang: str, server: str = 'zuma'):
+HOME = {
+	'pi': 'home/pi',
+	'sabondoc': 'users/ugrad/2020/winter/sabondoc',
+}
+
+def transfer(remote: str, sol: int, lang: str):
 	path_filename = Path('{0}.{1}'.format(SOLUTION, lang))
 	path_dst = (
 		Path('{0}:'.format(LOCAL))
@@ -28,18 +32,20 @@ def transfer(sol: int, lang: str, server: str = 'zuma'):
 		/ path_filename
 	)
 	path_src =  (
-		Path('sabondoc@{0}.{1}:'.format(server, REMOTE))
+		Path('{0}:'.format(remote))
+		/ Path(HOME[remote.split('@')[0]])
 		/ Path(DIR_REMOTE)
 		/ Path('{n:05d}'.format(n = sol))
 		/ Path(LANGUAGES[lang])
 		/ path_filename
 	)
 	subprocess.run(['scp', str(path_src), str(path_dst)], shell = True)
-		
+
 def main():
+	remote = str(input("Remote: "))
 	sol = int(input("Solution: "))
 	lang = str(input("Language: "))
-	transfer(sol, lang);
+	transfer(remote, sol, lang);
 			
 if __name__ == "__main__":
 	main()
