7a6ef8d38238bb0d0d0831ff490c61628012591f
sBondoc <sabondoc@uci.edu>
2022-03-19 23:27:20 -0700

    Added argparse support.

--- script/leet.py
+++ script/leet.py
@@ -6,6 +6,7 @@ for copy/paste to LeetCode via local browser.
 
 """
 
+import argparse
 import subprocess
 from pathlib import Path
 
@@ -24,15 +25,18 @@ HOME = {
 	'sabondoc': 'users/ugrad/2020/winter/sabondoc',
 }
 
-def transfer(remote: str, sol: int, lang: str):
+parser = argparse.ArgumentParser(description = "SCP LeetCode submission.")
+parser.add_argument('--port', help = "Optional port number.")
+
+def transfer(remote: str, port: str, sol: int, lang: str):
 	path_filename = Path('{0}.{1}'.format(SOLUTION, lang))
 	path_dst = (
-		Path('{0}:'.format(LOCAL))
+		Path('{0}:/'.format(LOCAL))
 		/ Path(DIR_LOCAL)
 		/ path_filename
 	)
 	usr = remote.split('@')[0]
-	path_src =  (
+	path_src = (
 		Path('{0}:'.format(remote))
 		/ Path(HOME[usr])
 		/ Path(DIR_REMOTE)
@@ -41,15 +45,46 @@ def transfer(remote: str, sol: int, lang: str):
 		/ path_filename
 	)
 	opt = ""
-	if (usr == "pi"):
-		opt = "-P {0}".format(int(input("Port: ")))
-	subprocess.run(['scp', opt, str(path_src), str(path_dst)], shell = True)
+	if (port != None):
+		opt = "-P {0} ".format(port)
+	cmd = "scp {0}{1} {2}".format(opt, str(path_src), str(path_dst))
+	print(cmd)
+	subprocess.run(cmd)
 
-def main():
-	remote = str(input("Remote: "))
-	sol = int(input("Solution: "))
-	lang = str(input("Language: "))
-	transfer(remote, sol, lang);
+def setup_parser() -> argparse.ArgumentParser:
+	ret = argparse.ArgumentParser(description = "SCP LeetCode submission from remote to local.")
+	ret.add_argument(
+		'--remote',
+		'-r',
+		type = str,
+		required = True,
+		help = "Remote server source (e.g. \"sabondoc@zuma.eecs.uci.edu\")."
+	)
+	ret.add_argument(
+		'--solution',
+		'-s',
+		required = True,
+		type = int,
+		help = "Solution number (e.g. \"69\")."
+	)
+	ret.add_argument(
+		'--language',
+		'-l',
+		required = True,
+		type = str,
+		help = "Language (e.g. \"c\")."
+	)
+	ret.add_argument(
+		'--port',
+		'-p',
+		required = False,
+		type = int,
+		help = "Port number for port forwarding (e.g. \"22\")."
+	)
+	return ret
+
+def main(args: argparse.Namespace = setup_parser().parse_args()):
+	transfer(args.remote, args.port, args.solution, args.language);
 			
 if __name__ == "__main__":
 	main()
